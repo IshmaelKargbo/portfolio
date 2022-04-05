67d77f72afdaa40756c247a618d7c6c79cba841a
Ishmael Kargbo <ishmaelkargbo5@outlook.com>
2022-04-05 16:50:32 +0000

    details popup window
    * You should implement the following interactions:
    - When the user clicks (or taps) the
    button to check project details, the popup with details about the project appears.
    - When the user clicks (or taps) the close (X) button, the popup disappears.
    * You should implement popups for both mobile and desktop screen sizes:
    - Mobile popup
    - Desktop popup

--- js/scripts.js
+++ js/scripts.js
@@ -19,21 +19,3 @@ navList.onclick = () => {
     mobileNav.style.display = "none";
     body.style.overflow = "auto";
 };
-
-// Project View
-const projectView = document.getElementById("project-view");
-const works = document.querySelectorAll("#see-project");
-const closeProject = document.getElementById("close-project-modal");
-const workCards = Array.from(works);
-
-workCards.forEach(element => {
-    element.onclick = (e) => {
-        projectView.style.display = 'block';
-        body.style.overflow = "hidden";
-    };
-})
-
-closeProject.onclick = () => {
-    projectView.style.display = "none";
-    body.style.overflow = "auto";
-};
