5b7a62b325a0f9d04cf831304f419f3d581b9d28
SoniaKarungi <98121137+SoniaKarungi@users.noreply.github.com>
2022-04-05 10:58:42 +0300

    Update scripts.js
    add modal interactivity to the project cards

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,34 @@ const body = document.querySelector("body");
 const navList = document.querySelector("#nav-list");
 
 mobileNavButton.onclick = () => {
-  body.style.overflow = "hidden";
-  mobileNav.style.display = "block";
+    body.style.overflow = "hidden";
+    mobileNav.style.display = "block";
 };
 
 closeModalButton.onclick = () => {
-  mobileNav.style.display = "none";
-  body.style.overflow = "auto";
+    mobileNav.style.display = "none";
+    body.style.overflow = "auto";
 };
 
 navList.onclick = () => {
-  mobileNav.style.display = "none";
-  body.style.overflow = "auto";
+    mobileNav.style.display = "none";
+    body.style.overflow = "auto";
 };
 
-// Project View 
+// Project View
+const projectView = document.getElementById("project-view");
+const works = document.querySelectorAll("#see-project");
+const closeProject = document.getElementById("close-project-modal");
+const workCards = Array.from(works);
 
+workCards.forEach(element => {
+    element.onclick = (e) => {
+        projectView.style.display = 'block';
+        body.style.overflow = "hidden";
+    };
+})
+
+closeProject.onclick = () => {
+    projectView.style.display = "none";
+    body.style.overflow = "auto";
+};
